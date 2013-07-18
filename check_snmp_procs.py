62aafaf0662e9e63c274a6f99ba5529ed9aa3822
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2013-07-18 11:35:12 +0200

    added some error handling and debugging features

--- check_snmp_procs.py
+++ check_snmp_procs.py
@@ -16,10 +16,11 @@ processes_obsessed = []
 processes_not_running = []
 processes = {}
 debug = 0
+verbose = 0
 
 # exit with unknown message and exit code, if anything went wrong
-def exitunknown():
-	print "UNKNOWN: unable to get running processes"
+def exit_unknown(error_message):
+	print "UNKNOWN: %s" % error_message
 	sys.exit(3)
 
 # execute system-calls
@@ -28,51 +29,76 @@ def execute(executable, params):
 		params = [params]
 	command = [executable]
 	command.extend(params)
+	if debug:
+		print "- executing: %s" % " ".join(command)
 	p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out = p.stdout.read()
 	err = p.stderr.read()
 	return p.wait(), out, err
 
 # query target-host to get a list of key-value pairs
-def snmpgetlist(oid):
+def get_list_by_snmp(oid):
         result = execute(command ,params + [oid])
         if result[0] == 0:
                 retval = str(result[1]).split('\n')
-                if len(retval) <= 2:
-                        exitunknown()
+		if debug:
+			print "- received %i values via snmp" % len(retval)
+                if len(retval) <= 2:	
+                        exit_unknown("received invalid data via snmp for oid %s" % oid)
+		if verbose:
+			print "received items: \n %s \n" % retval
                 return retval
         else:
-                exitunknown()
+                exit_unknown("something went wrong on getting data via snmp for oid %s" % oid)
 
 # convert raw list to dict of key-value pairs
-def convertlist(raw_list):
+def convert_list_to_kv_dict(raw_list):
 	dict = {}
 	for line in raw_list:
 		if line:
+			# split linestring into key and value
 			line = line.split('=')
+			# separate last oid element as pid
 			key = line[0].split('.')[-1].strip()
+			# remove unwanted characters
 			value = line[1].strip().strip('"')
+			# if there is anything left in value, add it to the dict with key=pid
 			if value:
 				dict[key] = value
 	return dict
 
 # get a list of processes and their parameters
-def getprocesses():
-	hrSWRunNameList = convertlist(snmpgetlist(hrSWRunName_oid))
-	hrSWRunParametersList = convertlist(snmpgetlist(hrSWRunParameters_oid))
+def get_processes():
+	hrSWRunNameList = convert_list_to_kv_dict(get_list_by_snmp(hrSWRunName_oid))
+	hrSWRunParametersList = convert_list_to_kv_dict(get_list_by_snmp(hrSWRunParameters_oid))
 	for key, value in hrSWRunParametersList.items():
-		hrSWRunNameList[key] = hrSWRunNameList[key] + " " + value
+		try:
+			hrSWRunNameList[key] = hrSWRunNameList[key] + " " + value
+		except KeyError:
+			exit_unknown("could not match parameter: %s to process with pid: %s" % (value,key)) 
+	if debug:
+		print "- created processes list with %d items" % len(hrSWRunNameList)
+	if verbose:
+		print "list of running processes: \n %s \n" % hrSWRunNameList
 	return hrSWRunNameList
 
 # check if a given string matches the list of running processes
-def checkrunning(searchstring):
+def check_running(searchstring):
         for process in processes.values():
                 if process.startswith(searchstring):
+			if debug:
+				print "- found match for process: %s and searchstring: %s" % (process,searchstring)
                         return 1
         return 0
 
+# split a string with csv values into a list of values
+def create_list_from_csv(string):
+	return string.strip().split(',')
+
 # parameter handling
 parser = argparse.ArgumentParser()
+parser.add_argument("--debug", help="show debug output", action="store_true")
+parser.add_argument("--verbose", help="show verbose output", action="store_true")
 parser.add_argument("--user", help="(if proto=v3) username", type=str)
 parser.add_argument("--password", help="(if proto=v3) password", type=str)
 parser.add_argument("hostname", help="hostname of system to query", type=str)
@@ -81,21 +107,40 @@ parser.add_argument("protocol", help="snmp protocol version [1|2c|3]", type=str)
 parser.add_argument("process_list", help="comma-separated list of processes", type=str)
 args = parser.parse_args()
 
+# check if debug or verbose option is set
+if args.debug:
+	print "debug mode on"
+	debug = 1
+if args.verbose:
+	print "verbose mode on"
+	verbose = 1
+
+# check if the correct snmp protocol version is given
+if not args.protocol in ["1","2c","3"]:
+	exit_unknown("invalid protocol parameter supplied, use: 1|2c|3")
+
 # create list of processes to search for
-processes_obsessed = args.process_list.strip().split(',')
+processes_obsessed = create_list_from_csv(args.process_list)
+if debug:
+	print "processes to search for: %s" % processes_obsessed
 
 # create params list
 params = ['-v' + args.protocol,'-Cr200','-Cn0','-OQns','-c' + args.community,args.hostname]
 if args.protocol == '3':
-	params.extend(['-lauthPriv', '-asha', '-xAES', '-u' + args.user, '-X' + args.password, '-A' + args.password])
+	try:
+		# append params for snmpv3 auth and encryption
+		params.extend(['-lauthPriv', '-asha', '-xAES', '-u' + args.user, '-X' + args.password, '-A' + args.password])
+	except TypeError:
+		exit_unknown("not all parameters given for snmp v3, define --user and --password")
 
-# get a list of running processes
-processes = getprocesses()
+# get list of running processes
+processes = get_processes()
 
 # check all processes needed to run
 for item in processes_obsessed:
-	if not checkrunning(item):
-		processes_not_running.append(item)
+	if item:
+		if not check_running(item):
+			processes_not_running.append(item)
 
 if len(processes_not_running) == 0:
 	print "OK: all processes running"
